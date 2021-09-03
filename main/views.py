baa4a518ed9119284a551a0072b24f9e0f71bc49
paguilaro <paula.aguilar@gmail.com>
2021-09-02 20:37:17 -0400

    registro usuarios

--- main/views.py
+++ main/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    context = {
-        'saludo': 'Hola'
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
 
+    else:
+        name = request.POST['name']
